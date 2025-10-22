2da728b969a758aab8700934e0f4e29fc213fb98
sarthakjain0906 <sarthak09.06.06@gmail.com>
2025-10-22 15:03:51 +0000

    show given pattern

---
+++ day23_pattern.c
@@ -0,0 +1,18 @@
+// Q46: Write a program to print the following pattern:
+// *****
+// *****
+// *****
+// *****
+// *****
+#include <stdio.h>
+int main() {
+    int i;
+    int j;
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
