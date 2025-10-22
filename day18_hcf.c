d59779dd84e9771d6ef590f615142ee674919576
sarthakjain0906 <sarthak09.06.06@gmail.com>
2025-10-22 13:47:17 +0000

    hcf of given number

---
+++ day18_hcf.c
@@ -0,0 +1,19 @@
+// Q36: Write a program to find the HCF (GCD) of two numbers.
+// Sample Test Cases:
+// Input 1: 12 18
+// Output 1: 6
+// Input 2: 7 9
+// Output 2: 1
+#include <stdio.h>
+int main() {
+    int a, b, i, hcf;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    for (i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0)
+            hcf = i;
+    }
+    printf("%d\n", hcf);
+    return 0;
+}
+
