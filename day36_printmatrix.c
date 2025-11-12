2fa4d4e141b8fd5ed49024c5f34357fdfa928595
sarthakjain0906 <sarthak09.06.06@gmail.com>
2025-11-12 16:49:38 +0000

    print the matrix

---
+++ day36_printmatrix.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    printf("Enter the elements of the matrix:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("The matrix is:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+/* Program to read and print a matrix */
