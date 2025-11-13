c92456f9e61675031407e631279a8aa3ea3486a1
sarthakjain0906 <sarthak09.06.06@gmail.com>
2025-11-13 16:24:47 +0000

    check two strings are anagram or not

---
+++ day47_anagrams.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+// Q93: Check if two strings are anagrams of each other
+
+int main() {
+    char str1[100], str2[100];
+    int count1[26] = {0}, count2[26] = {0};
+    int i, flag = 0;
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    for (i = 0; str1[i] != '\0'; i++) {
+        if (str1[i] >= 'a' && str1[i] <= 'z')
+            count1[str1[i] - 'a']++;
+        else if (str1[i] >= 'A' && str1[i] <= 'Z')
+            count1[str1[i] - 'A']++;
+    }
+
+    for (i = 0; str2[i] != '\0'; i++) {
+        if (str2[i] >= 'a' && str2[i] <= 'z')
+            count2[str2[i] - 'a']++;
+        else if (str2[i] >= 'A' && str2[i] <= 'Z')
+            count2[str2[i] - 'A']++;
+    }
+
+    for (i = 0; i < 26; i++) {
+        if (count1[i] != count2[i]) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("Anagrams\n");
+    else
+        printf("Not anagrams\n");
+
+    return 0;
+}
